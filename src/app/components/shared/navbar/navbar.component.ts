0123df5cda24988307663f439a23935d6d32994f
LeezMito <leezmitoo@gmail.com>
2019-05-02 17:49:34 -0500

    Primeros pasos
    Hola world

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  public clickLogin(){
+    this.auth.login();
+  }
+
+}
